2f71ba8635502de73de2de8be2ec1ff5b7989f74
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-11-25 12:06:28 +0000

    add task 3:convertToObject

---
+++ ObjectsAndClassesLab/convertToObject.js
@@ -0,0 +1,10 @@
+function convertToObject(jsonStr) {
+    let person = JSON.parse(jsonStr);
+
+    for (let [key, value] of Object.entries(person)) {
+        console.log(`${key}: ${value}`);
+    }
+
+}
+
+convertToObject('{"name": "George", "age": 40, "town": "Sofia"}')
