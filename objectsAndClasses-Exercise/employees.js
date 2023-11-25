2eca8fd1cd21508ba96f88da3192b878e3cf3cc9
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-11-25 12:38:39 +0000

    add task 1: employees

---
+++ objectsAndClasses-Exercise/employees.js
@@ -0,0 +1,19 @@
+function solve(namesArr) {
+  
+
+  let employees = {};
+  for (const name of namesArr) {
+    employees[name] = name.length;
+  }
+
+  Object.keys(employees).forEach((name) => {
+    console.log(`Name: ${name} -- Personal Number: ${employees[name]}`);
+  });
+}
+
+solve([
+  "Silas Butler",
+  "Adnaan Buckley",
+  "Juan Peterson",
+  "Brendan Villarreal",
+]);
