5cbf5257b6ce9311997d3b523e0279c5b5312d5f
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-11-25 13:25:01 +0000

    task 3 :sumNumbers

--- domIntroduction-Lab/03.Sum Numbers/SumNumbers.js
+++ domIntroduction-Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,9 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    
+    const firstElement = document.querySelector("#num1").value;
+    const secondElement = document.querySelector("#num2").value;
+
+    const result = Number(firstElement) + Number(secondElement);
+    
+    document.querySelector("#sum").value = result;
 }
