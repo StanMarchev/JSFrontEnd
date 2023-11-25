299c7149bac65700a84c40b3bbe8513407168649
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-11-25 12:51:10 +0000

    add task 5: inventory

---
+++ objectsAndClasses-Exercise/inventory.js
@@ -0,0 +1,25 @@
+function solve(input) {
+    let heroes = [];
+    input.forEach((line) => {
+      const [heroName, heroLevel, ...items] = line.split(" / ");
+      heroes.push({
+        Hero: heroName,
+        level: heroLevel,
+        Items: [...items].join(", "),
+      });
+    });
+  
+    const sortedHeroes = heroes.sort((a, b) => a.level - b.level);
+  
+    sortedHeroes.forEach((element) => {
+      console.log(
+        `Hero: ${element.Hero}\nlevel => ${element.level}\nitems => ${element.Items}`
+      );
+    });
+  }
+  
+  solve([
+    "Isacc / 25 / Apple, GravityGun",
+    "Derek / 12 / BarrelVest, DestructionSword",
+    "Hes / 1 / Desolator, Sentinel, Antara",
+  ]);
