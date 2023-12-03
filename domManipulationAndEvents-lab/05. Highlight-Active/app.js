e32d3ca1953aca16ad1dd36444f92a4c44c0f833
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-12-03 12:50:09 +0000

    task5-highlightActive - complete

--- domManipulationAndEvents-lab/05. Highlight-Active/app.js
+++ domManipulationAndEvents-lab/05. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focused() {
-    console.log('TODO:...');
+    const fields = Array.from(document.getElementsByTagName('input'));
+
+    for (const field of fields) {
+        field.addEventListener('focus', onfocus);
+        field.addEventListener('blur', onBlur);
+    }
+
+    function onfocus(e) {
+        const divParentElement = e.currentTarget.parentNode;
+        divParentElement.classList.add('focused');
+    }
+
+    function onBlur (e) {
+        const divParentElement = e.currentTarget.parentNode;
+        divParentElement.classList.remove('focused');
+    }
 }
