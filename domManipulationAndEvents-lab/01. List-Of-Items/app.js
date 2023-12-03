af122277ee8ede01f6f166fce3a960fc61f5bd09
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-12-03 11:54:20 +0000

    list-of-items-solution

--- domManipulationAndEvents-lab/01. List-Of-Items/app.js
+++ domManipulationAndEvents-lab/01. List-Of-Items/app.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputElement = document.querySelector('#newItemText');
+
+    const newListItem = document.createElement('li');
+    newListItem.textContent = inputElement.value;
+
+    const ulList = document.querySelector('#items');
+    ulList.appendChild(newListItem);
+
+    inputElement.value=' '
+
+   
 }
