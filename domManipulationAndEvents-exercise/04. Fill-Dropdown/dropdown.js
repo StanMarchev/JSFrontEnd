650ea097d1b9bf0db89e2ba3aa075da94e094433
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-12-03 15:18:22 +0000

    Update dropdown.js

--- domManipulationAndEvents-exercise/04. Fill-Dropdown/dropdown.js
+++ domManipulationAndEvents-exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,17 @@
 function addItem() {
-    console.log('TODO:...');
+    const newItem = document.getElementById('newItemText')
+    const newValue = document.getElementById('newItemValue')
+    const select = document.getElementById('menu')
+
+    console.log(select);
+
+    const newItemValue = newItem.value
+    const newValueValue = newValue.value
+    const option = document.createElement('option')
+    option.textContent = newItemValue
+    option.value = newValueValue
+    select.appendChild(option)
+
+    newItem.value = ''
+    newValue.value = ''
 }
