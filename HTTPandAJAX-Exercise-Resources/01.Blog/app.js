66ace38dba689804116d654afe0dd758d182aeff
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-12-12 20:10:31 +0000

    finishTask01

--- HTTPandAJAX-Exercise-Resources/01.Blog/app.js
+++ HTTPandAJAX-Exercise-Resources/01.Blog/app.js
@@ -6,7 +6,7 @@ function attachEvents() {
     const postsSelect = document.getElementById('posts');
     const postTitle = document.getElementById('post-title');
     const postBody = document.getElementById('post-body');
-    const postComments = document.getElementById('post-coments');
+    const postComments = document.getElementById('post-comments');
 
     let allPosts = {};
 
@@ -29,18 +29,29 @@ function attachEvents() {
 
     });
 
-    viewButton.addEventListener('click', async ()=> {
+    viewButton.addEventListener('click', async () => {
+
+        postBody.innerHTML = '';
+        postComments.innerHTML = '';
 
         const postId = postsSelect.value;
 
         postBody.textContent = allPosts[postId].body;
         postTitle.textContent = allPosts[postId].title;
-    })
 
-    
+        const response = await fetch(baseURL + 'comments');
+        const commentsInfo = await response.json();
+        
+        const filteredComments = Object.values(commentsInfo).filter(e => e.postId === postId);
+        filteredComments.forEach(comment => {
 
+            const li = document.createElement('li');
+            li.id = comment.id;
+            li.textContent = comment.text;
+            postComments.appendChild(li);
+        });
 
-
+    });
 
 }
 
