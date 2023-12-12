c7cd8e435ff1c9e7e2c2a9d97c40074bb5e6b938
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-12-12 19:45:23 +0000

    loadPostsButton

--- HTTPandAJAX-Exercise-Resources/01.Blog/app.js
+++ HTTPandAJAX-Exercise-Resources/01.Blog/app.js
@@ -1,5 +1,37 @@
 function attachEvents() {
-    // TODO:
+    
+    const baseURL = 'http://localhost:3030/jsonstore/blog/'
+    const loadPostsButton = document.getElementById('btnLoadPosts');
+    const viewButton = document.getElementById('btnViewPost');
+    const postsSelect = document.getElementById('posts');
+    const postTitle = document.getElementById('post-title');
+    const postBody = document.getElementById('post-body');
+    const postComments = document.getElementById('post-coments');
+
+    let allPosts = {};
+
+
+    loadPostsButton.addEventListener('click', async () => {
+
+        postsSelect.innerHTML = '';
+
+        const response = await fetch(baseURL + 'posts')
+        allPosts = await response.json();
+
+        for (const [postId, postObj] of Object.entries(allPosts)) {
+            const option = document.createElement('option');
+
+            option.value = postId;
+            option.textContent = postObj.title;
+            postsSelect.appendChild(option);
+        }
+        
+
+    }) 
+
+
+
+
 }
 
 attachEvents();
